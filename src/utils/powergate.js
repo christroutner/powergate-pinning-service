8978fad63acaf4e6b0c5d73e2a57860c1545e17b
Chris Troutner <chris.troutner@gmail.com>
2021-01-20 13:53:47 -0800

    Using VPS

--- src/utils/powergate.js
+++ src/utils/powergate.js
@@ -6,7 +6,7 @@ import {
 var pow;
 
 // const POW_HOST = "http://localhost:6002"; // or whatever powergate instance you want
-const POW_HOST = "http://0.0.0.0:6002"; // or whatever powergate instance you want
+const POW_HOST = "http://104.248.76.169:6002"; // or whatever powergate instance you want
 
 export const getPowergateInstance = () => {
   if (pow) {
